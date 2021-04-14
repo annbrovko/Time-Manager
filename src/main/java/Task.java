1b9533f2d626e7fdb428ba1656ce3d105c493f3f
annbrovko <55671523+annbrovko@users.noreply.github.com>
2021-04-14 02:08:51 +0200

    Add formatting to the toString method

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -7,16 +7,13 @@ public class Task {
     private final Duration duration;
     private final DateTime deadline;
     private final DateTime latestStart;
-    private String deadlineToStr;
-    private String durationToStr;
-    private String latestStartToStr;
 
     // create an object Task with four properties title, duration, priority and deadline
     public Task (String title, Duration duration, int priority, DateTime deadline, DateTime latestStart){
         this.title = title;
         this.duration = duration;
-        this.priority = priority;
         this.deadline = deadline;
+        this.priority = priority;
         this.latestStart = latestStart;
     }
 /*
@@ -31,13 +28,19 @@ public class Task {
     }
     public DateTime getDeadline(){ return this.deadline; }
 */
+    TimeData timeData = new TimeData();
+
     // convert list to string
     public String toString(){
-        String result;
-        deadlineToStr = this.deadline.toString("dd/MM/yyyy HH:mm");
-        durationToStr = this.duration.toStandardHours().toString();
-        latestStartToStr = this.latestStart.toString("dd/MM/yyyy HH:mm");
-        result = "\nTask title: " + this.title + "\nTask priority: " + this.priority + "\nTask duration: " + this.durationToStr + "\nTask deadline: " + this.deadlineToStr + "\nLatest start time: " + this.latestStartToStr;
-        return result;
+        // format deadline for print
+        String printDeadline = this.deadline.toString("dd/MM/yyyy HH:mm");
+        // format duration for print
+        String durationToMinutes = this.duration.toStandardMinutes().toString();
+        String clean = durationToMinutes.replaceAll("\\D+","");
+        int durationUser = Integer.parseInt(clean);
+        // format latest start for print
+        String printLatestStart = this.latestStart.toString("dd/MM/yyyy HH:mm");
+        // print a string with item data
+        return "\nTask title: " + this.title + "\nTask priority: " + this.priority + "\nTask duration: " + timeData.convertToHoursAndDays(durationUser) + "\nTask deadline: " + printDeadline + "\nLatest start time: " + printLatestStart;
     }
 }
